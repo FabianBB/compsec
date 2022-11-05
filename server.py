0b913ba29423a704f94e26a3f3697d7d164166fd
IgnacioCadarso <72268339+IgnacioCadarso@users.noreply.github.com>
2022-11-05 18:51:49 +0100

    Fixed issue

--- server.py
+++ server.py
@@ -18,15 +18,16 @@ print('Waiting...')
 ServerSocket.listen(5)
 HashTable = {}
 HashTable_count = {}
+HashTable_numberOfConnections = {}  # To count how many instances of the same user are connected at a given time
 
 def threaded_client(connection):
-    #Receive json
+    # Receive json
     jsonReceived = connection.recv(1024)
     data = jsonReceived.decode('utf-8')
     data = json.loads(data)
     print("Json received -->", data)
 
-    #Save elements of json
+    # Save elements of json
     name = data["id"]
     password = data["password"]
     steps = data["actions"]["steps"]
@@ -35,11 +36,12 @@ def threaded_client(connection):
     name = name
     password = hashlib.sha256(str.encode(password)).hexdigest()  # Password hash using SHA256
 
-    #Registration phase
+    # Registration phase
     if name not in HashTable:
         HashTable[name] = password
         HashTable_count[name] = 0
         connection.send(str.encode('Registration Successful'))
+        HashTable_numberOfConnections[name] = 1
         print('Registered : ', name)
         print("{:<8} {:<20}".format('USER', 'PASSWORD'))
         for k, v in HashTable.items():
@@ -50,23 +52,22 @@ def threaded_client(connection):
     else:
         # If already existing user, check if the entered password is correct
         if (HashTable[name] == password):
-            # read from log file
-            with open("log.txt", 'r') as log:
-                loglines = log.read().split("\n")
-            log.close()
-            # find line with last transaction of a given user and extract the counter
-            s = name + ","
-            indices = [s in line for line in loglines]
-            idx = max([i for i, x in enumerate(indices) if x])
-            HashTable_count[name] = int(loglines[idx].split(",")[1])
             connection.send(str.encode('Connection Successful'))  # Response Code for Connected Client
             print('Connected : ', name)
             print("-------------------------------------------")
+            HashTable_numberOfConnections[name] = HashTable_numberOfConnections[name] + 1
             counter_phase(name, steps, delay)
         else:
             connection.send(str.encode('Login Failed'))  # Response code for login failed
             print('Connection denied : ', name)
     connection.close()
+    HashTable_numberOfConnections[name] = HashTable_numberOfConnections[name] - 1
+    if HashTable_numberOfConnections[name] <= 0:
+        print("COUNTER RESET")
+        HashTable_count[name] = 0  # Reset counter
+
+
+
 # Handles the counter
 def counter_phase(name, steps, delay):
     for step in steps:
@@ -80,7 +81,6 @@ def counter_phase(name, steps, delay):
         log.close()
         print("Counter: ", name, HashTable_count[name])
         time.sleep(delay)
-    HashTable_count[name] = 0 #Reset counter
 
 while True:
     Client, address = ServerSocket.accept()
